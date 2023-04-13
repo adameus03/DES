4582f299902662821b9c243138a846c5b7a3d708
Amadeusz Sitnicki <amadeusz.sitnicki1@gmail.com>
2023-04-13 07:02:49 +0200

    Removed test results from display

--- main.cpp
+++ main.cpp
@@ -9,28 +9,10 @@ using namespace std;
 typedef unsigned char uchar;
 
 void usage(char* argv0){
-    checker(test_initial_perm(), "initial_perm");
-    checker(test_final_perm(), "final_perm");
-    checker(test_e_selection(), "e_selection");
-    checker(test_sbox_combined(), "sbox_combined");
-    checker(test_p_permutation(), "p_permutation");
-    checker(test_permuted_choice_1(), "permuted_choice_1");
-    checker(test_permuted_choice_2(), "permuted_choice_2");
-    checker(test_xor_blks(), "xor_blks");
-    checker(test_lshift_blk7(), "lshift_blk7");
-
-    //visual_test_feistel_f();
-    //visual_test_encrypt();
-
-    checker(test_feistel_f(), "feistel_f");
-    checker(test_encrypt_decrypt_blk(), "encrypt_blk, decrypt_blk, get_subkeys");
-
-    std::cout << endl;
-
-    std::cout << "Usage: " << argv0 << " {-e | -d} <input_file_path> <output_file_path> [key_file_path] [-r <key_store_file>] " << std::endl;
+    std::cout << std::endl <<"Usage: " << argv0 << " {-e | -d} <input_file_path> <output_file_path> [key_file_path] [-r <key_store_file>] " << std::endl;
 }
 
-/* Main function */
+
 int main(int argc, char** argv)
 {
 
@@ -69,7 +51,21 @@ int main(int argc, char** argv)
 }
 
 
+/*checker(test_initial_perm(), "initial_perm");
+    checker(test_final_perm(), "final_perm");
+    checker(test_e_selection(), "e_selection");
+    checker(test_sbox_combined(), "sbox_combined");
+    checker(test_p_permutation(), "p_permutation");
+    checker(test_permuted_choice_1(), "permuted_choice_1");
+    checker(test_permuted_choice_2(), "permuted_choice_2");
+    checker(test_xor_blks(), "xor_blks");
+    checker(test_lshift_blk7(), "lshift_blk7");
 
+    //visual_test_feistel_f();
+    //visual_test_encrypt();
+
+    checker(test_feistel_f(), "feistel_f");
+    checker(test_encrypt_decrypt_blk(), "encrypt_blk, decrypt_blk, get_subkeys");*/
 
 
 
